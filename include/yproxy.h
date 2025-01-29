7b2ff47c23653fd40b7ae1fc91c5ee9d6ae05a88
Victor Kremensky <victorkremensky@mail.ru>
2025-01-29 12:16:38 +0000

    Firstly, we did not remove the code, and secondly, it will happen again.

--- include/yproxy.h
+++ include/yproxy.h
@@ -3,16 +3,30 @@
 
 #include "chunkinfo.h"
 #include "io_adv.h"
-#include "ydeleter.h"
-#include "ylister.h"
-#include "yreader.h"
-#include "ywriter.h"
 #include <memory>
 #include <string>
 #include <vector>
 
+class YproxyConnector {
+public:
+  explicit YproxyConnector() = 0;
+  ~YproxyConnector();
+  virtual bool close();
+
+
+protected:
+  int prepareYproxyConnection();
+  std::shared_ptr<IOadv> adv_{nullptr};
+  ssize_t segindx_{0};
+
+  int client_fd_{-1};
+
+}
+
+
+
 /* reader using yproxy */
-class YProxyReader : public YReader {
+class YProxyReader : public YproxyConnector {
 public:
   friend class ExternalWriter;
   explicit YProxyReader(std::shared_ptr<IOadv> adv, ssize_t segindx,
@@ -20,59 +34,45 @@ public:
   ~YProxyReader();
 
 public:
-  virtual bool close();
   virtual bool read(char *buffer, size_t *amount);
 
   virtual bool empty();
 
-public:
 protected:
   /* prepare connection for chunk reading */
-  int prepareYproxyConnection(const ChunkInfo &ci, size_t start_off);
   std::vector<char> ConstructCatRequest(const ChunkInfo &ci, size_t start_off);
 
 private:
-  std::shared_ptr<IOadv> adv_{nullptr};
-  ssize_t segindx_{0};
   int64_t order_ptr_{0};
   const std::vector<ChunkInfo> order_;
   int64_t current_chunk_remaining_bytes_{0};
   int64_t current_chunk_offset_{0};
-
-  int client_fd_{-1};
 
   int current_retry{0};
   int retry_limit{1};
 };
 
 // Write into external storage using yproxy
-class YProxyWriter : public YWriter {
+class YProxyWriter : public YproxyConnector {
 public:
   explicit YProxyWriter(std::shared_ptr<IOadv> adv, ssize_t segindx,
                         ssize_t modcount, const std::string &storage_path);
 
   virtual ~YProxyWriter();
 
-  virtual bool close();
-
   virtual bool write(const char *buffer, size_t *amount);
 
 protected:
   /* prepare connection for chunk reading */
-  int prepareYproxyConnection();
   std::vector<char> ConstructPutRequest(std::string fileName);
   std::vector<char> ConstructCopyDataRequest(const char *buffer, size_t amount);
 
 private:
   std::string createXPath();
 
-  std::shared_ptr<IOadv> adv_;
-  ssize_t segindx_;
   ssize_t modcount_;
   XLogRecPtr insertion_rec_ptr_;
   std::string storage_path_;
-
-  int client_fd_{-1};
 
 public:
   std::string getExternalStoragePath() { return storage_path_; }
@@ -81,7 +81,7 @@ public:
 };
 
 /* Delete specified file from external storage, bypassing all sanity checks */
-class YProxyDeleter : public YDeleter {
+class YProxyDeleter : public YproxyConnector {
 public:
   /*
    * Direct delete dispatch, appliable from MASTER
@@ -95,23 +95,15 @@ public:
 
   virtual ~YProxyDeleter();
 
-  virtual bool close();
-
   virtual bool deleteChunk(const std::string &chunkName);
 
 protected:
   /* prepare connection for chunk reading */
-  int prepareYproxyConnection();
   std::vector<char> ConstructDeleteRequest(std::string fileName);
 
 private:
-  std::shared_ptr<IOadv> adv_;
-  ssize_t segindx_{-1};
-
   bool garbage_cleanup_{false};
   bool confirm_{false};
-
-  int client_fd_{-1};
 };
 
 // list external storage using yproxy
@@ -121,14 +113,11 @@ public:
 
   virtual ~YProxyLister();
 
-  virtual bool close();
-
   virtual std::vector<storageChunkMeta> list_relation_chunks();
   virtual std::vector<std::string> list_chunk_names();
 
 protected:
   std::vector<char> ConstructListRequest(std::string fileName);
-  int prepareYproxyConnection();
 
   struct message {
     char type;
@@ -137,10 +126,4 @@ protected:
   };
   message readMessage();
   std::vector<storageChunkMeta> readObjectMetaBody(std::vector<char> *body);
-
-private:
-  std::shared_ptr<IOadv> adv_;
-  ssize_t segindx_;
-
-  int client_fd_{-1};
 };
