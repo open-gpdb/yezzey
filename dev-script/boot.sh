922a423e2c7b34cf282356577991ef1bcb2403f2
reshke <reshke@double.cloud>
2025-02-01 18:06:24 +0500

    Update boot.sh (#86)

--- dev-script/boot.sh
+++ dev-script/boot.sh
@@ -1,25 +1,14 @@
 
 export GPHOME=/usr/local/gpdb/
-source gpAux/gpdemo/gpdemo-env.sh
 source /usr/local/gpdb/greenplum_path.sh
-
-
 
 make clean && make -j32 install
 make destroy-demo-cluster && make create-demo-cluster
-gpconfig -c yezzey.storage_prefix -v "'wal-e/6'"
-gpconfig -c yezzey.storage_bucket -v "'gpyezzey'"
-
-gpconfig -c yezzey.yproxy_socket -v "'/tmp/yproxy.sock'"
-gpconfig -c yezzey.use_gpg_crypto -v "true"
+source gpAux/gpdemo/gpdemo-env.sh
 
 gpconfig -c max_worker_processes -v 10
-
 gpconfig -c yezzey.autooffload -v  "on"
-
 gpconfig -c shared_preload_libraries -v yezzey
-
 gpstop -a -i && gpstart -a
-
 
 createdb $USER
