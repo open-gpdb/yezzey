2cb59b3a3ce332f0a86c732a282b9cf7b18d4fa7
reshke <reshke@double.cloud>
2025-02-01 17:49:25 +0500

    Update conf_setup.sh: remove ununed params from conf_setup.sh

--- dev-script/conf_setup.sh
+++ dev-script/conf_setup.sh
@@ -1,25 +1,15 @@
 
 export GPHOME=/usr/local/gpdb/
-source gpAux/gpdemo/gpdemo-env.sh
 source /usr/local/gpdb/greenplum_path.sh
-
-
 
 make clean && make -j32 install
 make destroy-demo-cluster && make create-demo-cluster
-gpconfig -c yezzey.storage_prefix -v "'wal-e/mdbrhqjnl6k5duk7loi2/6'"
-gpconfig -c yezzey.storage_bucket -v "'gptest'"
-gpconfig -c yezzey.storage_config -v "'/home/reshke/s3test.conf'"
-gpconfig -c yezzey.storage_host -v "'https://storage.yandexcloud.net'"
-
-gpconfig -c max_worker_processes -v 10
+source gpAux/gpdemo/gpdemo-env.sh
 
 gpconfig -c yezzey.autooffload -v  "on"
-
 gpconfig -c shared_preload_libraries -v yezzey
 
 gpstop -a -i && gpstart -a
 
 
 createdb $USER
-psql postgres -f ./gpcontrib/yezzey/test/regress/yezzey.sql
