8d1c8698b3371a2dc0e8885b1e5814bedf130b98
Victor Kremensky <victorkremensky@mail.ru>
2025-02-04 12:55:46 +0000

    rebase

--- include/msgproto.h
+++ include/msgproto.h
@@ -29,23 +29,31 @@ const size_t UINT64_SZ = 8;
 
 class MsgBuilder {
 public:
-  explicit MsgBuilder();
-  // description
-  MsgBuilder &fieldMessageType();
-  MsgBuilder &fieldUInt64();
-  MsgBuilder &fieldString(ssize_t val);
-  MsgBuilder &endDescription();
-  // place values
-  MsgBuilder &addMessageType(char message_type);
-  MsgBuilder &addUInt64(uint64_t val);
-  MsgBuilder &addString(std::string str);
-  std::vector<char> get();
-
+    explicit MsgBuilder();
+    // description
+    MsgBuilder& fieldMessageType();
+    MsgBuilder& fieldUInt64();
+    MsgBuilder& fieldString(ssize_t len);
+    MsgBuilder& fieldProto();
+    MsgBuilder& fieldBytes(ssize_t len);
+    MsgBuilder& endDescription();
+    // place values
+    MsgBuilder& addMessageType(char message_type);
+    MsgBuilder& addUInt64(uint64_t val);
+    MsgBuilder& addString(std::string str);
+    MsgBuilder& addBytes(const char* bytes, ssize_t len);
+    MsgBuilder& addProto(char farg);
+    MsgBuilder& addProto(char farg,char sarg);
+    MsgBuilder& addProto(char farg,char sarg,char targ);
+    MsgBuilder& addProto(char farg,char sarg,char targ, char larg);
+    std::vector<char> get();
 protected:
-  ssize_t putUInt64(uint64_t val, ssize_t padding);
-  ssize_t putString(std::string val, ssize_t padding);
-  ssize_t putChar(char c, ssize_t padding);
-  ssize_t length;
-  ssize_t cursor;
-  std::vector<char> data;
+    ssize_t putUInt64(uint64_t val, ssize_t padding);
+    ssize_t putString(std::string val, ssize_t padding);
+    ssize_t putChar(char c, ssize_t padding);
+    ssize_t putProto(char farg,char sarg,char targ, char larg,ssize_t cursor);
+    ssize_t putBytes(const char* bytes,ssize_t len,ssize_t padding);
+    ssize_t length;
+    ssize_t cursor;
+    std::vector<char> data;
 };
