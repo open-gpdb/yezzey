8d75a4ea0f9a25e788dc10036cb80a47bdda369d
reshke <reshke@double.cloud>
2025-02-06 18:51:01 +0500

    Refactor common infra code (#99)

--- src/yproxy_connector.cpp
+++ src/yproxy_connector.cpp
@@ -41,3 +41,68 @@ int YProxyConnector::prepareYproxyConnection() {
   }
   return 0;
 }
+
+int commonReadRFQResponce(int client_fd_) {
+  int len = MSG_HEADER_SIZE;
+  char buffer[len];
+  // try to read small number of bytes in one op
+  // if failed, give up
+  int rc = ::read(client_fd_, buffer, len);
+  if (rc != len) {
+    // handle
+    return -1;
+  }
+
+  uint64_t msgLen = 0;
+  for (int i = 0; i < 8; i++) {
+    msgLen <<= 8;
+    msgLen += uint8_t(buffer[i]);
+  }
+
+  if (msgLen != MSG_HEADER_SIZE + PROTO_HEADER_SIZE) {
+    // protocol violation
+    return 1;
+  }
+
+  // substract header
+  msgLen -= len;
+
+  char data[msgLen];
+  rc = ::read(client_fd_, data, msgLen);
+  if (rc < 0) {
+    return -1;
+  }
+  if (uint64_t(rc) != msgLen) {
+    // handle
+    return -1;
+  }
+
+  if (data[0] != MessageTypeReadyForQuery) {
+    return 2;
+  }
+  return 0;
+}
+
+
+int commonWriteFull(int client_fd_, const std::vector<char> &msg) {
+  int len = msg.size();
+  int sync_offset = 0;
+  while (len > 0) {
+    auto rc = ::write(client_fd_, msg.data() + sync_offset, len);
+
+    if (rc <= 0) {
+      // handle
+      return -1;
+    }
+    len -= rc;
+    sync_offset += rc;
+  }
+  return 0;
+}
+
+
+std::vector<char> CommonCostructCommandCompleteRequest() {
+  MsgBuilder builder = MsgBuilder().fieldProto().endDescription();
+
+  return builder.addProto(MessageTypeCommandComplete).get();
+}
