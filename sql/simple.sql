d7a9cccf1325a7db0c272db9a945687e27c68a35
diphantxm <asefru78@mail.ru>
2025-02-14 10:28:15 +0000

    check external bytes

--- sql/simple.sql
+++ sql/simple.sql
@@ -24,9 +24,9 @@ SELECT count(1) FROM regaoty;
 
 SELECT * FROM regaoty ORDER BY i LIMIT 5 OFFSET 7823;
 
-SELECT segindex FROM yezzey_offload_relation_status('regaoty');
-SELECT segindex,segfileindex FROM yezzey_offload_relation_status_per_filesegment('regaoty');
-SELECT segindex,segfileindex FROM yezzey_relation_describe_external_storage_structure('regaoty');
+SELECT segindex,external_bytes FROM yezzey_offload_relation_status('regaoty');
+SELECT segindex,segfileindex,external_bytes FROM yezzey_offload_relation_status_per_filesegment('regaoty');
+SELECT segindex,segfileindex,external_bytes FROM yezzey_relation_describe_external_storage_structure('regaoty');
 
 SELECT count(), sum(external_bytes) FROM yezzey_offload_relation_status('regaoty') WHERE offload_reloid='regaoty'::regclass::oid;
 SELECT count(), sum(external_bytes) FROM yezzey_offload_relation_status_per_filesegment('regaoty') WHERE offload_reloid='regaoty'::regclass::oid;
