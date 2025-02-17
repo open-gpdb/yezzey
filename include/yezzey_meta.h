d99609a3a8987b8e65c66ae4053fe3022f067c30
Yury Frolov <yura200253@gmail.com>
2025-02-17 17:54:49 +0500

    Change signature && add defines

--- include/yezzey_meta.h
+++ include/yezzey_meta.h
@@ -13,5 +13,5 @@
 EXTERNC void YezzeyUpdateMetadataRelations(
     Oid yandexoid /*yezzey auxiliary index oid*/, Oid reloid,
     Oid relfilenodeOid, int64_t blkno, int64_t offset_start,
-    int64_t offset_finish, int32_t encrypted, int32_t reused, int64_t modcount,
+    int64_t offset_finish, bool encrypted, bool kek, int32_t reused, int64_t modcount,
     XLogRecPtr lsn, const char *x_path /* external path */, const char *md5);
--- src/meta.cpp
+++ src/meta.cpp
@@ -3,10 +3,10 @@
 void YezzeyUpdateMetadataRelations(
     Oid yandexoid /*yezzey auxiliary index oid*/, Oid reloid,
     Oid relfilenodeOid, int64_t blkno, int64_t offset_start,
-    int64_t offset_finish, int32_t encrypted, int32_t reused, int64_t modcount,
+    int64_t offset_finish, bool encrypted, bool kek, int32_t reused, int64_t modcount,
     XLogRecPtr lsn, const char *x_path /* external path */, const char *md5) {
   YezzeyVirtualIndexInsert(yandexoid, reloid, relfilenodeOid, blkno,
-                           offset_start, offset_finish, encrypted, reused,
+                           offset_start, offset_finish, int32_t(YEZZEY_IS_ENC * encrypted + YEZZEY_ENC_KEK * kek), reused,
                            modcount, lsn, x_path);
   /* TODO: update yezzey relfilemap */
 }
