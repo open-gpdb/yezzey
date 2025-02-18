459f6cd49160dfffb452e290af0bea6daa9b2c3c
Yury Frolov <yura200253@gmail.com>
2025-02-18 13:17:40 +0500

    Refactor YezzeyUpdateMetadataRelations

--- src/meta.cpp
+++ src/meta.cpp
@@ -5,8 +5,13 @@ void YezzeyUpdateMetadataRelations(
     Oid relfilenodeOid, int64_t blkno, int64_t offset_start,
     int64_t offset_finish, bool encrypted, bool kek, int32_t reused, int64_t modcount,
     XLogRecPtr lsn, const char *x_path /* external path */, const char *md5) {
+  int32_t flags = 0;
+  if (encrypted)
+    flags |= YEZZEY_IS_ENC;
+  if (kek)
+    flags |= YEZZEY_ENC_KEK;
   YezzeyVirtualIndexInsert(yandexoid, reloid, relfilenodeOid, blkno,
-                           offset_start, offset_finish, int32_t(YEZZEY_IS_ENC * encrypted + YEZZEY_ENC_KEK * kek), reused,
+                           offset_start, offset_finish, flags, reused,
                            modcount, lsn, x_path);
   /* TODO: update yezzey relfilemap */
 }
