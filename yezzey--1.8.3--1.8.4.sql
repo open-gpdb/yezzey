ef4ee5f3ac9aa66916c24c230d5c9ce036b866e6
Victor Kremensky <victorkremensky@mail.ru>
2025-02-21 11:51:20 +0000

    sql for upgrade

--- yezzey--1.8.3--1.8.4.sql
+++ yezzey--1.8.3--1.8.4.sql
@@ -1,3 +1,27 @@
 
 
 -- create yezzey hint index here
+
+
+CREATE OR REPLACE FUNCTION yezzey.yezzey_binary_upgrade_1_8_3_to_1_8_4_m() RETURNS void
+AS 'MODULE_PATHNAME','yezzey_binary_upgrade_1_8_3_to_1_8_4'
+VOLATILE
+EXECUTE ON MASTER
+LANGUAGE C STRICT;
+
+
+CREATE OR REPLACE FUNCTION yezzey.yezzey_binary_upgrade_1_8_3_to_1_8_4_seg() RETURNS void  
+AS 'MODULE_PATHNAME','yezzey_binary_upgrade_1_8_3_to_1_8_4'
+VOLATILE
+EXECUTE ON ALL SEGMENTS
+LANGUAGE C STRICT;
+
+SET allow_segment_dml TO ON;
+
+SELECT yezzey.yezzey_binary_upgrade_1_8_3_to_1_8_4_seg();
+SELECT yezzey.yezzey_binary_upgrade_1_8_3_to_1_8_4_m();
+
+RESET allow_segment_DML;
+
+DROP FUNCTION yezzey.yezzey_binary_upgrade_1_8_3_to_1_8_4_seg();
+DROP FUNCTION yezzey.yezzey_binary_upgrade_1_8_3_to_1_8_4_m();
