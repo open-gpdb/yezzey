f4122eeee6de58cfba2fa469d2668387b7a4ed5a
Victor <87538976+visill@users.noreply.github.com>
2025-02-21 18:40:21 +0800

    simplify 1.8--1.8.1 and 1.8.2-1.8.3 sqls (#114)
    * simplify 1.8-1.8.1
    
    * simplify 1.8.2-1.8.3

--- yezzey--1.8.2--1.8.3.sql
+++ yezzey--1.8.2--1.8.3.sql
@@ -1,25 +1,15 @@
 
-
-CREATE OR REPLACE FUNCTION yezzey.yezzey_binary_upgrade_1_8_2_to_1_8_3() RETURNS void
-AS 'MODULE_PATHNAME'
-VOLATILE
-EXECUTE ON ALL SEGMENTS
-LANGUAGE C STRICT;
-
-
-CREATE OR REPLACE FUNCTION yezzey.yezzey_binary_upgrade_1_8_2_to_1_8_3_m() RETURNS void
+CREATE OR REPLACE FUNCTION yezzey.yezzey_binary_upgrade_1_8_2_to_1_8_3_m() RETURNS VOID
 AS 'MODULE_PATHNAME','yezzey_binary_upgrade_1_8_2_to_1_8_3'
 VOLATILE
-EXECUTE ON MASTER
-LANGUAGE C STRICT;
+LANGUAGE C STRICT
+EXECUTE ON MASTER;
 
 
-CREATE OR REPLACE FUNCTION yezzey.yezzey_binary_upgrade_1_8_2_to_1_8_3_seg() 
-RETURNS VOID AS 
-$$
-SELECT yezzey.yezzey_binary_upgrade_1_8_2_to_1_8_3();
-$$ 
-LANGUAGE SQL 
+CREATE OR REPLACE FUNCTION yezzey.yezzey_binary_upgrade_1_8_2_to_1_8_3_seg() RETURNS VOID 
+AS 'MODULE_PATHNAME','yezzey_binary_upgrade_1_8_2_to_1_8_3'
+VOLATILE
+LANGUAGE C STRICT
 EXECUTE ON ALL SEGMENTS;
 
 SELECT yezzey.yezzey_binary_upgrade_1_8_2_to_1_8_3_seg();
