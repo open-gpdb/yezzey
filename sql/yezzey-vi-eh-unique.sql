d582a04db3b92132d32511f42afc87db8ea27de0
Victor Kremensky <victorkremensky@mail.ru>
2025-03-07 12:24:59 +0000

    cleanup in regress

--- sql/yezzey-vi-eh-unique.sql
+++ sql/yezzey-vi-eh-unique.sql
@@ -5,3 +5,9 @@ ALTER EXTENSION yezzey UPDATE TO '1.8.3';
 INSERT INTO yezzey.yezzey_virtual_index (filenode,blkno,modcount) VALUES (1,1,1);
 
 INSERT INTO yezzey.yezzey_virtual_index (filenode,blkno,modcount) VALUES (1,1,1);
+
+TRUNCATE yezzey.yezzey_virtual_index;
+
+DROP EXTENSION yezzey;
+
+CHECKPOINT;
