60a7525178f5e924f88b031646cc6b3e4a0af1de
Victor Kremensky <victorkremensky@mail.ru>
2025-03-07 11:50:37 +0000

    fix test for new tree

--- sql/yezzey-vi-eh-unique.sql
+++ sql/yezzey-vi-eh-unique.sql
@@ -2,6 +2,6 @@ CREATE EXTENSION yezzey ;
 
 ALTER EXTENSION yezzey UPDATE TO '1.8.3';
 
-INSERT INTO yezzey.yezzey_virtual_index (filenode,blkno) VALUES (1,1);
+INSERT INTO yezzey.yezzey_virtual_index (filenode,blkno,modcount) VALUES (1,1,1);
 
-INSERT INTO yezzey.yezzey_virtual_index (filenode,blkno) VALUES (1,1);
+INSERT INTO yezzey.yezzey_virtual_index (filenode,blkno,modcount) VALUES (1,1,1);
