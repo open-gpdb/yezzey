e0703be5d2036058f4173b4a9ec48a45428d3084
Victor Kremensky <victorkremensky@mail.ru>
2025-03-07 08:37:16 +0000

    add test

---
+++ sql/yezzey-vi-eh-unique.sql
@@ -0,0 +1,7 @@
+CREATE EXTENSION yezzey ;
+
+ALTER EXTENSION yezzey UPDATE TO '1.8.3';
+
+INSERT INTO yezzey.yezzey_virtual_index (filenode,blkno) VALUES (1,1);
+
+INSERT INTO yezzey.yezzey_virtual_index (filenode,blkno) VALUES (1,1);
