54714816488f475c6f0c0d78fade3e9af57ccfb6
reshke kirill <reshke@double.cloud>
2025-04-24 12:16:01 +0000

    Fix compile warn

--- src/expire_hint.cpp
+++ src/expire_hint.cpp
@@ -61,8 +61,8 @@ yezzey_create_expire_hint_idx_internal(Oid relid, const std::string &relname,
 
   /* ShareLock is not really needed here, but take it anyway */
   auto yezzey_rel = heap_open(YEZZEY_EXPIRE_HINT_RELATION, ShareLock);
-  char *colname_x_path = "x_path";
-  auto indexColNames = list_make1(colname_x_path);
+  const char *colname_x_path = "x_path";
+  auto indexColNames = list_make1((void *) colname_x_path);
 
   auto indexInfo = makeNode(IndexInfo);
 
