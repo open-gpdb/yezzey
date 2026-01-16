363bad5598b3beaa0b63e921811fcef4357d823f
Victor <87538976+visill@users.noreply.github.com>
2026-01-16 17:45:33 +0500

    Better delete offloaded files in otm (#159)

--- src/xvacuum.cpp
+++ src/xvacuum.cpp
@@ -9,7 +9,8 @@
 #include <string>
 #include <url.h>
 #include <util.h>
-
+#include "storage.h"
+#include "offload_tablespace_map.h"
 /*
  * yezzey_delete_chunk_internal:
  * Given external chunk path, remove it from external storage
@@ -70,36 +71,44 @@ int yezzey_vacuum_garbage_internal(int segindx, bool confirm, bool crazyDrop) {
   return 0;
 }
 
-int yezzey_vacuum_garbage_relation_internal(Relation rel, int segindx,
+int yezzey_vacuum_garbage_relation_internal(Relation aorel, int segindx,
                                             bool confirm, bool crazyDrop) {
   try {
-    auto ioadv = std::make_shared<IOadv>(
-        "", "", std::string(storage_class /*storage_class*/),
-        multipart_chunksize, DEFAULTTABLESPACE_OID, "" /* coords */,
-        InvalidOid /* reloid */, use_gpg_crypto, yproxy_socket);
+    auto rnode = aorel->rd_node;
 
     auto tp = SearchSysCache1(NAMESPACEOID,
-                              ObjectIdGetDatum(RelationGetNamespace(rel)));
+                              ObjectIdGetDatum(RelationGetNamespace(aorel)));
 
     if (!HeapTupleIsValid(tp)) {
-      elog(ERROR, "yezzey: failed to get namescape name of relation %d",
-           RelationGetNamespace(rel));
+      elog(ERROR, "yezzey: failed to get namescape name of relation %s",
+           RelationGetRelationName(aorel));
     }
 
-    relnodeCoord coords{DEFAULTTABLESPACE_OID, rel->rd_node.dbNode,
-                        rel->rd_node.relNode, segindx};
-    Form_pg_namespace nsptup = (Form_pg_namespace)GETSTRUCT(tp);
-    auto nspname = std::string(nsptup->nspname.data);
-    std::string relname = RelationGetRelationName(rel);
+    auto nsptup = (Form_pg_namespace)GETSTRUCT(tp);
+    auto nspname = std::string(NameStr(nsptup->nspname));
+
+    auto spcNode = resolveTablespaceOidByName(
+      YezzeyGetRelationOriginTablespace(NULL, NULL, RelationGetRelid(aorel)));
+    
+    relnodeCoord coords{spcNode, rnode.dbNode,
+                        rnode.relNode, segindx};
+    relnodeCoord coords_old{DEFAULTTABLESPACE_OID, rnode.dbNode,
+                        rnode.relNode, segindx};
+    ReleaseSysCache(tp);
+
+    std::string relname = RelationGetRelationName(aorel);
 
     std::string storage_path(
         yezzey_block_db_file_path(nspname, relname, coords, segindx));
-
+    std::string storage_path_old(
+        yezzey_block_db_file_path(nspname, relname, coords_old, segindx));
+    auto ioadv = std::make_shared<IOadv>(
+        nspname, relname, std::string(storage_class),
+        multipart_chunksize, coords, aorel->rd_id, use_gpg_crypto, yproxy_socket);
     auto deleter = std::make_shared<YProxyDeleter>(ioadv, ssize_t(segindx),
                                                    confirm, crazyDrop);
-    ReleaseSysCache(tp);
 
-    if (deleter->deleteChunk(storage_path)) {
+    if (deleter->deleteChunk(storage_path) && deleter->deleteChunk(storage_path_old)) {
       return 0;
     }
 
