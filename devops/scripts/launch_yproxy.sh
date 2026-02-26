910fa4c173e760e96f208014054de99fae2b8d08
Leonid <63977577+leborchuk@users.noreply.github.com>
2026-02-26 12:06:51 +0300

    Do not use tablespace map in open-gpdb tests (#184)
    We use credentialsMap for store credentials in yproxy.
    
    But static credentials are set only for default tablespace - `gpyezzey`. Others two use default credentials provider. Default credentials provider is not set (in a Docker) correctly, so queries hang forever and we could see a numerous messages `resigning` in yproxy.log
    
    yproxy process also waits for syn_sent answer for non-existence default credentials host
    
    ```
    yproxy  53531 gpadmin   13u     IPv4          264361572      0t0       TCP cdw:45054-><ip>:80 (SYN_SENT)
    yproxy  53531 gpadmin   14u     IPv4          264345463      0t0       TCP cdw:43130-><ip>:80 (SYN_SENT)
    yproxy  53531 gpadmin   15u     IPv4          264410117      0t0       TCP cdw:58594-><ip>:80 (SYN_SENT)
    yproxy  53531 gpadmin   16u     IPv4          264361571      0t0       TCP cdw:58602-><ip>:80 (SYN_SENT)
    yproxy  53531 gpadmin   17u     IPv4          264378210      0t0       TCP cdw:38558-><ip>:80 (SYN_SENT)
    yproxy  53531 gpadmin   18u     IPv4          264361581      0t0       TCP cdw:38560-><ip>:80 (SYN_SENT)
    ```
    
    Also yproxy connect to gp database while performing delete garbage, so we need to be started under gpadmin in order to be able to connect.
    
    ---------
    
    Co-authored-by: Leonid Borchuk <xifos@qavm-f9b691f5.qemu>

---
+++ devops/scripts/launch_yproxy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+          set -eo pipefail
+          set -x
+
+          rm -rf /tmp/yproxy.sock
+          #run yproxy in daemon mode
+          /usr/bin/yproxy -c /tmp/yproxy.yaml -ldebug > yproxy.log 2>&1 &
+
+          i=0
+          while (! [ -S /tmp/yproxy.sock ]) && [ $i -lt 20 ]; do sleep 1; i=$(($i+1)) ; done
+
+          if (! [ -S /tmp/yproxy.sock ]); then
+             echo "::error::Config yproxy failed"
+             exit 1
+          fi
+
+          chmod 777 /tmp/yproxy.sock
