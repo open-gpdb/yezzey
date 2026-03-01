f54098379637a8a53398c147790402348232edd7
reshke <reshke@double.cloud>
2026-03-01 10:39:34 +0000

    Make regression output for few tests better

--- sql/yezzey-vacuum_cbdb.sql
+++ sql/yezzey-vacuum_cbdb.sql
@@ -7,6 +7,7 @@ insert into vacuum_aot select * from generate_series(1, 10000);
 delete from vacuum_aot;
 insert into vacuum_aot select * from generate_series(10000, 20000);
 VACUUM vacuum_aot;
+ANALYZE vacuum_aot;
 select count(1) from vacuum_aot; -- works ok, use second block(segment) of relation ;
 insert into vacuum_aot select * from generate_series(10000, 20000); -- insert goes in first block(segment) with modcount 4
 select count(1) from vacuum_aot; -- works
