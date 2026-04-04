8be87fa961b24a59ce7a1448673347db66774f22
reshke <reshke@double.cloud>
2026-04-04 14:11:34 +0000

    f

--- src/offload_policy.cpp
+++ src/offload_policy.cpp
@@ -237,8 +237,8 @@ bool YezzeySetRelationExpiritySeg(Oid i_reloid, int i_relpolicy,
 
     auto offtuple = heap_form_tuple(RelationGetDescr(offrel), values, nulls);
 
-    simple_heap_update(offrel, &oldtuple->t_self, offtuple);
 #if IsGreenplum6
+    simple_heap_update(offrel, &oldtuple->t_self, offtuple);
     CatalogUpdateIndexes(offrel, offtuple);
 #else
     CatalogTupleUpdate(offrel, &oldtuple->t_self, offtuple);
