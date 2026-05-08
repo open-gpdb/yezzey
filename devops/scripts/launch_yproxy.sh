0a098be6e9df209b9eef085c7b54b7cc356e9247
reshke <reshke@double.cloud>
2026-05-08 11:51:52 +0500

    Update launch_yproxy.sh

--- devops/scripts/launch_yproxy.sh
+++ devops/scripts/launch_yproxy.sh
@@ -6,7 +6,7 @@
           rm -rf /tmp/yproxy.sock
           #run yproxy in daemon mode
           mkdir  /tmp/data
-          /usr/bin/yproxy -c /tmp/yproxy.yaml -ldebug > yproxy.log 2>&1 &
+          /usr/bin/yproxy -c /tmp/yproxy.yaml -ldebug > /tmp/data/yproxy.log 2>&1 &
 
           i=0
           while (! [ -S /tmp/yproxy.sock ]) && [ $i -lt 20 ]; do sleep 1; i=$(($i+1)) ; done
