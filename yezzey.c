5882a119af5ed4684a8dc1cfdea84b23ec5f534c
Andrey Sokolov <sokolov.andrey.yurevich@gmail.com>
2026-09-17 10:31:25 +0300

    Add check for yezzey is in shared_preload_libraries (#272)
    Yezzey must be in shared_preload_libraries. There was an error on creating the
    extension when yezzey was missing from shared_preload_libraries. Now we can
    get an informative message.

--- yezzey.c
+++ yezzey.c
@@ -1568,6 +1568,11 @@ void yezzey_TrackObjDrop(Relation rel) {
 void _PG_init(void) {
   /* Allocate shared memory for yezzey workers */
 
+  if (!process_shared_preload_libraries_in_progress)
+    ereport(ERROR,
+            (errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+             errmsg("yezzey must be registered in shared_preload_libraries")));
+
 #if IsModernYezzey
   allow_in_place_tablespaces = true;
 #endif
