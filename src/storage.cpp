46ed6db3b0ce5e36d8f4e19a4d19eeca3b458606
Vladislav Shchetinin <45269644+Vlasdislav@users.noreply.github.com>
2026-09-25 15:17:43 +0300

    Get rid of code duplication
    Co-authored-by: Andrey Sokolov <sokolov.andrey.yurevich@gmail.com>

--- src/storage.cpp
+++ src/storage.cpp
@@ -265,14 +265,8 @@ void loadRelationSegment(Relation aorel, Oid loadSpcOid, Oid orig_relnode,
   }
 
   elog(yezzey_ao_log_level, "contructed path %s", path);
-  if (ensureFilepathLocal(path)) {
-    if (local_path) {
-      pfree(local_path);
-    }
-    return;
-  }
-
-  loadSegmentFromExternalStorage(aorel, nspname, relname, segno, coords, path);
+  if (!ensureFilepathLocal(path))
+    loadSegmentFromExternalStorage(aorel, nspname, relname, segno, coords, path);
   if (local_path) {
     pfree(local_path);
   }
